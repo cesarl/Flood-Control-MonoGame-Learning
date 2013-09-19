df998202efd891a52d99ae28b42e4a1e1340e1c1
Cesar Leblic <cesar.leblic@gmail.com>
2013-09-19 10:11:18 +0200

    FadingPiece class

---
+++ floodControl/floodControl/FadingPiece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace floodControl
+{
+    class FadingPiece : GamePiece
+    {
+        public float alphaLevel = 1.0f;
+        public static float rate = 0.02f;
+
+        public FadingPiece(string type, string suffix) :
+            base(type, suffix)
+        { }
+
+        public void Update()
+        {
+            alphaLevel - MathHelper.Max(0, alphaLevel - rate);
+        }
+    }
+}
