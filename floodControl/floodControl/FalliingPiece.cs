2b5b74e7be0d341e043eec77a6e30fe1500a4205
Cesar Leblic <cesar.leblic@gmail.com>
2013-09-19 09:55:04 +0200

    FallingPiece class

---
+++ floodControl/floodControl/FalliingPiece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace floodControl
+{
+    class FalliingPiece : GamePiece
+    {
+        public int offset;
+        public static int fallRate = 5;
+
+        public FalliingPiece(string type, int offset) : base(type)
+        {
+            this.offset = offset;
+        }
+
+        public void Update()
+        {
+            offset = (int)MathHelper.Max(0, offset - fallRate);
+        }
+    }
+}
